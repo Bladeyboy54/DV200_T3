c945e62f9e56f6936a02d600453e54aae1607c8e
Bladeyboy54 <221146@virtualwindow.co.za>
2023-08-18 16:53:58 +0200

    Nav bar fixed

--- dv200t3/src/components/navbar.js
+++ dv200t3/src/components/navbar.js
@@ -1,46 +1,52 @@
 import React from 'react';
-import {
-  MDBNavbar,
-  MDBContainer,
-  MDBNavbarBrand,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBBtn,
-  MDBNavbarNav,
-  MDBInputGroup
-} from 'mdb-react-ui-kit';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function BasicNav() {
   return (
     <>
-      <MDBNavbar expand='lg' light bgColor='light' style={{height: '100px'}}>
-        <div style={{ display: 'flex', flexDirection: 'column'}}>
-          <MDBContainer fluid style={{height: '60px'}}>
-            <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
-            <MDBNavbarNav className='mb-2 mb-lg-0'>
-              <MDBNavbarItem>
-                <MDBNavbarLink active aria-current='page' href='#'>
-                  Home
-                </MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Link</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-                  Disabled
-                </MDBNavbarLink>
-              </MDBNavbarItem>
-            </MDBNavbarNav>
-          </MDBContainer>
-          <MDBContainer fluid style={{height: '40px', float: 'right'}}>
-            <MDBInputGroup tag="form" className='d-flex mb-3'>
-                <input className='form-control' placeholder="Type query" aria-label="Search" type='Search' style={{width: '20%'}} />
-                <MDBBtn outline>Search</MDBBtn>
-              </MDBInputGroup>
-          </MDBContainer>
-        </div>
-      </MDBNavbar>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="#">Tiger Technologies</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#" disabled>
+                Link
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
--- dv200t3/src/pages/landing.js
+++ dv200t3/src/pages/landing.js
@@ -3,7 +3,6 @@ import {
     MDBCarousel,
     MDBCarouselItem,
   } from 'mdb-react-ui-kit';
-import '../css/landing.css';
 
 function Landing (){
 
@@ -44,7 +43,73 @@ function Landing (){
         </MDBCarouselItem>
       </MDBCarousel>
       <br />
-      <div  style={{backgroundColor: "#F5F5F5", width: "90%", marginLeft: "auto", marginRight: "auto"}}>
+      <div  style={{backgroundColor: "#F5F5F5", width: "90%", marginLeft: "auto", marginRight: "auto", borderRadius: "7px"}}>
+        <div class="row" style={{width: "80%", marginLeft: "auto", marginRight: "auto", paddingTop: "50px",  paddingBottom: "50px"}}>
+          <h1>Test</h1>
+          <div class="col-sm-3">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="https://thimg-afcc.kxcdn.com/@wth-img/225x225/products/2020/12/08/th00498291-1.jpg" alt="" class="img-fluid"/>
+                <a href="#!">
+                <div class="mask" style={{backgroundColor: "rgba(251, 251, 251, 0.15)"}}></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">$ 129.99</h5>
+                <p class="card-text">View more...</p>
+                <a href="#!" class="btn btn-primary">Buy Now</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7FHLQDQh5GUPK0XZA9lYoGjo8YUkjuq_iS-cbOkbvCtZdLi-KST7UjMUguh1Qa2IyjUs&usqp=CAU" alt="" class="img-fluid"/>
+                <a href="#!">
+                <div class="mask" style={{backgroundColor: "rgba(251, 251, 251, 0.15)"}}></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">$ 250.00</h5>
+                <p class="card-text">View more...</p>
+                <a href="#!" class="btn btn-primary">Buy Now</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqzmlIIPMxIaaXt0PCpfgzl1Dy81tfGckrK7ykm2Q2a3lJU0L_CcpidwbcifSToa3-rb8&usqp=CAU" alt="" class="img-fluid"/>
+                <a href="#!">
+                <div class="mask" style={{backgroundColor: "rgba(251, 251, 251, 0.15)"}}></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">$ 189.99</h5>
+                <p class="card-text">View more...</p>
+                <a href="#!" class="btn btn-primary">Buy Now</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqzmlIIPMxIaaXt0PCpfgzl1Dy81tfGckrK7ykm2Q2a3lJU0L_CcpidwbcifSToa3-rb8&usqp=CAU" alt="" class="img-fluid"/>
+                <a href="#!">
+                <div class="mask" style={{backgroundColor: "rgba(251, 251, 251, 0.15)"}}></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">$ 189.99</h5>
+                <p class="card-text">View more...</p>
+                <a href="#!" class="btn btn-primary">Buy Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br/>
+      <div  style={{backgroundColor: "#F5F5F5", width: "90%", marginLeft: "auto", marginRight: "auto", borderRadius: "7px"}}>
         <div class="row" style={{width: "80%", marginLeft: "auto", marginRight: "auto", paddingTop: "50px",  paddingBottom: "50px"}}>
           <h1>Test</h1>
           <div class="col-sm-3">
