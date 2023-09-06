21642b13c977e85de5c4336616f7abf02d34516b
Bladeyboy54 <221146@virtualwindow.co.za>
2023-09-06 12:58:05 +0200

    Updated navbar.js

--- dv200t3/src/components/navbar.js
+++ dv200t3/src/components/navbar.js
@@ -1,62 +1,53 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn
+} from "mdb-react-ui-kit";
 import Logo from "./logomain.svg";
 
-export default function BasicNav() {
+function BasicNav() {
+
+  const navbarStyle = {
+    display: "flex",
+    alignItems: "center"
+  };
+
   return (
-    <>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container fluid>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src={Logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            React Bootstrap
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    <MDBNavbar expand="lg" className="bg-body-tertiary">
+      <div className="container">
+        <MDBNavbarBrand href="#home">
+          <img
+            alt=""
+            src={Logo}
+            width="60"
+            className="d-inline-block align-top"
+          />{" "}
+          Tiger Technologies
+        </MDBNavbarBrand>
+        <MDBNavbarNav style={navbarStyle}  navbarScroll>
+          <MDBNavbarItem>
+            <MDBNavbarLink to="#products">Products</MDBNavbarLink>
+          </MDBNavbarItem>
+        </MDBNavbarNav>
+        <MDBNavbarNav className="ms-auto" style={{paddingRight: "0"}}>
+          <MDBNavbarItem >
+            <MDBNavbarLink to="#">
+              <span className="text-dark">Hello, User</span>
+            </MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink to="/cart">
+              <MDBBtn variant="outline-light">Cart</MDBBtn>
+            </MDBNavbarLink>
+          </MDBNavbarItem>
+        </MDBNavbarNav>
+      </div>
+    </MDBNavbar>
   );
 }
+
+export default BasicNav;
