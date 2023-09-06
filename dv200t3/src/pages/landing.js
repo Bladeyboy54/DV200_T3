90019812c0505aeb315045e0a22bfac24968a286
Bladeyboy54 <221146@virtualwindow.co.za>
2023-09-06 17:57:20 +0200

    cards are automatically populated(landing.js)

--- dv200t3/src/pages/landing.js
+++ dv200t3/src/pages/landing.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
+import { 
+  MDBCarousel,
+  MDBCarouselItem,
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 import axios from "axios";
 // import '../css/landing.css';
 
@@ -14,6 +23,17 @@ function Landing() {
       .then((response) => setGraphicsData(response.data))
       .catch((error) => console.error("Error fetching graphics data:", error));
   }, []);
+
+  function shuffleArray(array) {
+    let shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const x = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[x]] = [shuffledArray[x], shuffledArray[x]];
+    }
+    return shuffledArray;
+  }
+
+  const shuffledGraphicsData = shuffleArray(graphicsData);
 
   return (
     <>
@@ -54,6 +74,7 @@ function Landing() {
         </MDBCarouselItem>
       </MDBCarousel>
       <br />
+      {/* ///////////////////////////////////////////////////////////////////////// */}
       <div
         style={{
           backgroundColor: "#F5F5F5",
@@ -74,117 +95,23 @@ function Landing() {
           }}
         >
           <h1>Newest Products</h1>
-          <div class="col-sm-3">
-            <div class="card">
-              <div
-                class="bg-image hover-overlay ripple"
-                data-mdb-ripple-color="light"
-              >
-                <img
-                  src="https://thimg-afcc.kxcdn.com/@wth-img/225x225/products/2020/12/08/th00498291-1.jpg"
-                  alt=""
-                  class="img-fluid"
-                />
-                <a href="#!">
-                  <div
-                    class="mask"
-                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                  ></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">$ 129.99</h5>
-                <p class="card-text">View more...</p>
-                <a href="#!" class="btn btn-primary">
-                  Buy Now
-                </a>
-              </div>
+          {shuffledGraphicsData.map((graphic, index) => (
+            <div key={index} className="col-sm-3">
+              <MDBCard>
+                <MDBCardImage src={graphic.imageUrl} alt={graphic.name} />
+                <MDBCardBody>
+                  <MDBCardTitle>{graphic.name}</MDBCardTitle>
+                  <MDBCardText>{graphic.type}</MDBCardText>
+                  <MDBCardText>R{graphic.price.toFixed(2)}</MDBCardText>
+                  <MDBBtn color="primary">Buy Now</MDBBtn>
+                </MDBCardBody>
+              </MDBCard>
             </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="card">
-              <div
-                class="bg-image hover-overlay ripple"
-                data-mdb-ripple-color="light"
-              >
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7FHLQDQh5GUPK0XZA9lYoGjo8YUkjuq_iS-cbOkbvCtZdLi-KST7UjMUguh1Qa2IyjUs&usqp=CAU"
-                  alt=""
-                  class="img-fluid"
-                />
-                <a href="#!">
-                  <div
-                    class="mask"
-                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                  ></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">$ 250.00</h5>
-                <p class="card-text">View more...</p>
-                <a href="#!" class="btn btn-primary">
-                  Buy Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="card">
-              <div
-                class="bg-image hover-overlay ripple"
-                data-mdb-ripple-color="light"
-              >
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqzmlIIPMxIaaXt0PCpfgzl1Dy81tfGckrK7ykm2Q2a3lJU0L_CcpidwbcifSToa3-rb8&usqp=CAU"
-                  alt=""
-                  class="img-fluid"
-                />
-                <a href="#!">
-                  <div
-                    class="mask"
-                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                  ></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">$ 189.99</h5>
-                <p class="card-text">View more...</p>
-                <a href="#!" class="btn btn-primary">
-                  Buy Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="card">
-              <div
-                class="bg-image hover-overlay ripple"
-                data-mdb-ripple-color="light"
-              >
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqzmlIIPMxIaaXt0PCpfgzl1Dy81tfGckrK7ykm2Q2a3lJU0L_CcpidwbcifSToa3-rb8&usqp=CAU"
-                  alt=""
-                  class="img-fluid"
-                />
-                <a href="#!">
-                  <div
-                    class="mask"
-                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                  ></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">$ 189.99</h5>
-                <p class="card-text">View more...</p>
-                <a href="#!" class="btn btn-primary">
-                  Buy Now
-                </a>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <br />
+      {/* //////////////////////////////////////////////////////////////////////// */}
       <div
         style={{
           backgroundColor: "#F5F5F5",
@@ -205,114 +132,19 @@ function Landing() {
           }}
         >
           <h1>Now on Sale !!</h1>
-          <div class="col-sm-3">
-            <div class="card">
-              <div
-                class="bg-image hover-overlay ripple"
-                data-mdb-ripple-color="light"
-              >
-                <img
-                  src="https://thimg-afcc.kxcdn.com/@wth-img/225x225/products/2020/12/08/th00498291-1.jpg"
-                  alt=""
-                  class="img-fluid"
-                />
-                <a href="#!">
-                  <div
-                    class="mask"
-                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                  ></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">$ 129.99</h5>
-                <p class="card-text">View more...</p>
-                <a href="#!" class="btn btn-primary">
-                  Buy Now
-                </a>
-              </div>
+          {shuffledGraphicsData.map((graphic, index) => (
+            <div key={index} className="col-sm-3">
+              <MDBCard>
+                <MDBCardImage src={graphic.imageUrl} alt={graphic.name} />
+                <MDBCardBody>
+                  <MDBCardTitle>{graphic.name}</MDBCardTitle>
+                  <MDBCardText>{graphic.type}</MDBCardText>
+                  <MDBCardText>R{graphic.price.toFixed(2)}</MDBCardText>
+                  <MDBBtn color="primary">Buy Now</MDBBtn>
+                </MDBCardBody>
+              </MDBCard>
             </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="card">
-              <div
-                class="bg-image hover-overlay ripple"
-                data-mdb-ripple-color="light"
-              >
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7FHLQDQh5GUPK0XZA9lYoGjo8YUkjuq_iS-cbOkbvCtZdLi-KST7UjMUguh1Qa2IyjUs&usqp=CAU"
-                  alt=""
-                  class="img-fluid"
-                />
-                <a href="#!">
-                  <div
-                    class="mask"
-                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                  ></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">$ 250.00</h5>
-                <p class="card-text">View more...</p>
-                <a href="#!" class="btn btn-primary">
-                  Buy Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="card">
-              <div
-                class="bg-image hover-overlay ripple"
-                data-mdb-ripple-color="light"
-              >
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqzmlIIPMxIaaXt0PCpfgzl1Dy81tfGckrK7ykm2Q2a3lJU0L_CcpidwbcifSToa3-rb8&usqp=CAU"
-                  alt=""
-                  class="img-fluid"
-                />
-                <a href="#!">
-                  <div
-                    class="mask"
-                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                  ></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">$ 189.99</h5>
-                <p class="card-text">View more...</p>
-                <a href="#!" class="btn btn-primary">
-                  Buy Now
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="card">
-              <div
-                class="bg-image hover-overlay ripple"
-                data-mdb-ripple-color="light"
-              >
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqzmlIIPMxIaaXt0PCpfgzl1Dy81tfGckrK7ykm2Q2a3lJU0L_CcpidwbcifSToa3-rb8&usqp=CAU"
-                  alt=""
-                  class="img-fluid"
-                />
-                <a href="#!">
-                  <div
-                    class="mask"
-                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-                  ></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">$ 189.99</h5>
-                <p class="card-text">View more...</p>
-                <a href="#!" class="btn btn-primary">
-                  Buy Now
-                </a>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
